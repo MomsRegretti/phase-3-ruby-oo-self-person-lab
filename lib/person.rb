0d86d8a65859a3bc7264c58d5fb576025cb23dc3
CharlieRojas <crc_185@yahoo.com>
2022-05-05 00:35:37 -0500

    commit tree ef0af944e31000a9e53b73c177547c016832bca9

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,15 @@
-# your code goes here
+class Person
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account
+
+    def happiness=(happiness=8)
+        @happiness = happiness
+    end
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness
+        @hygiene = 8
+    end
+end
